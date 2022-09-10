af7a2d870f5a097c382d36c2eff84b4ffd64f8cb
pnichols789 <pnichols789@gmail.com>
2022-09-10 14:18:47 -0500

    -

--- Products-List/app.js
+++ Products-List/app.js
@@ -6,26 +6,44 @@ const products = [
 		price: 549,
 		thumbnail: "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
 	},
+    {
+        id: 2,
+        title: 'iPhone X',
+        description:
+          'SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...',
+        price: 899,
+        discountPercentage: 17.94,
+        rating: 4.44,
+        stock: 34,
+        brand: 'Apple',
+        category: 'smartphones',
+        thumbnail: 'https://dummyjson.com/image/i/products/2/thumbnail.jpg',
+        images: [
+          'https://dummyjson.com/image/i/products/2/1.jpg',
+          'https://dummyjson.com/image/i/products/2/2.jpg',
+          'https://dummyjson.com/image/i/products/2/3.jpg',
+          'https://dummyjson.com/image/i/products/2/thumbnail.jpg',
+        ],
+      },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const productList = products.map((product) => {
-    return `
+const productList = products.map((product) => `
         <article class="product-item">
-            <img src="https://dummyjson.com/image/i/products/1/thumbnail.jpg" alt="iPhone 9" class="photo">
+            <img src="${product.thumbnail}" alt="${product.title}" class="photo">
             <div class="item-info">
             <header>
-                <h4>iPhone 9</h4>
+                <h4>${product.title}</h4>
             </header>
-            <p class="item-text">An apple mobile which is nothing like apple</p>
-            <h4 class="price">$549</h4>
+            <p class="item-text">${product.description}</p>
+            <h4 class="price">$${product.price}</h4>
             </div>
         </article>
-    ` ;
-});
+    ` 
+);
 
 
 
 
-
-sectionCenter.innerHTML = productList;
+// removes comma from array
+sectionCenter.innerHTML = productList.join('');
