ab58932de652edf70bdd9b23dfbb710b6d23fc96
pnichols789 <pnichols789@gmail.com>
2022-09-10 13:38:56 -0500

    -

--- Products-List/app.js
+++ Products-List/app.js
@@ -1,11 +1,25 @@
 const products = [
-    {
-        id: 1,
-        title: 'iPhone 9',
-        description: 'An apple mobile which is nothing like apple',
-        price: 549,
-        thumbnail: 'https://dummyjson.com/image/i/products/1/thumbnail.jpg',
-    },
+	{
+		id: 1,
+		title: "iPhone 9",
+		description: "An apple mobile which is nothing like apple",
+		price: 549,
+		thumbnail: "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
+	},
 ];
 
-const sectionCenter = document.querySelector('.section-center');
+const sectionCenter = document.querySelector(".section-center");
+const productList = `
+    <article class="product-item">
+        <img src="https://dummyjson.com/image/i/products/1/thumbnail.jpg" alt="iPhone 9" class="photo">
+        <div class="item-info">
+        <header>
+            <h4>iPhone 9</h4>
+        </header>
+        <p class="item-text">An apple mobile which is nothing like apple</p>
+        <h4 class="price">$549</h4>
+        </div>
+    </article>
+`;
+
+sectionCenter.innerHTML = productList;
