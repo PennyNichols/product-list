b7938cec8f0178fc19779e3d49b8c2de325583f5
pnichols789 <pnichols789@gmail.com>
2022-09-10 13:28:15 -0500

    -

---
+++ Products-List/app.js
@@ -0,0 +1,11 @@
+const products = [
+    {
+        id: 1,
+        title: 'iPhone 9',
+        description: 'An apple mobile which is nothing like apple',
+        price: 549,
+        thumbnail: 'https://dummyjson.com/image/i/products/1/thumbnail.jpg',
+    },
+];
+
+const sectionCenter = document.querySelector('.section-center');
