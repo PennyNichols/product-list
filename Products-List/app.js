419263573bfbae6a21699560bb6a1176cecc61ee
pnichols789 <pnichols789@gmail.com>
2022-09-10 15:21:05 -0500

    -

--- Products-List/app.js
+++ Products-List/app.js
@@ -1,27 +1,42 @@
-import { products } from "./products.js";
-
-const sectionCenter = document.querySelector(".section-center");
+import { products } from './products.js';
+const sectionCenter = document.querySelector('.section-center');
+const buttonContainer = document.querySelector('.btn-container');
 const productList = products.map(
-	(product) => `
-        <article class="product-item">
-            <img src="${product.thumbnail}" alt="${product.title}" class="photo">
-            <div class="item-info">
-            <header>
-                <h4>${product.title}</h4>
-            </header>
-            <p class="item-text">${product.description}</p>
-            <h4 class="price">$${product.price}</h4>
-            </div>
-        </article>
-    `
+  (product) => `
+    <article class="product-item">
+      <img
+        src="${product.thumbnail}"
+        alt="${product.title}"
+        class="photo"
+      />
+      <div class="item-info">
+        <header>
+          <h4>${product.title}</h4>
+        </header>
+        <p class="item-text">${product.description}</p>
+        <h4 class="price">$${product.price}</h4>
+      </div>
+    </article>
+  `
 );
+sectionCenter.innerHTML = productList.join('');
+const categories = products.reduce(
+  (acc, product) => {
+    if (!acc.includes(product.category)) {
+      acc.push(product.category);
+    }
+    return acc;
+  },
+  ['All']
+);
+const categoryButtons = categories.map(
+  (category) =>
+    `<button type="button" class="filter-btn" data-id="${category}">${category}</button>`
+);
+buttonContainer.innerHTML = categoryButtons.join('');
 
-const categories = products.reduce((acc, product) => {
-	if (!acc.includes(product.category)) {
-		acc.push(product.category);
-	}
-	return acc;
-}, []);
+const filterButtons = document.querySelectorAll('.filter-btn');
 
-// removes comma from array
-sectionCenter.innerHTML = productList.join("");
+
+
+
